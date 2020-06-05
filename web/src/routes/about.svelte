655d3c6779525214fff14f2d4a0695ab58d06ea7
salvatoredangelo <salvatoredangelo@protonmail.com>
2020-06-05 15:00:25 -0400

    customized template a bit of a bit

--- web/src/routes/about.svelte
+++ web/src/routes/about.svelte
@@ -1,7 +1,25 @@
+<script>
+  import { onMount } from 'svelte'
+
+  let canvas
+
+  onMount(() => {
+    function setup() {
+      canvas = createCanvas(200, 200)
+    }
+
+    function draw() {
+      background(200, 200, 0)
+    }
+  })
+</script>
+
 <svelte:head>
-	<title>About</title>
+  <title>About</title>
 </svelte:head>
 
 <h1>About this site</h1>
 
 <p>This is the 'about' page. There's not much here.</p>
+
+<div id="sketch" />
